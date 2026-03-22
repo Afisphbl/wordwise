f09e1e1d094ed721a77173bde9e3729e19beffe6
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-03-22 14:30:19 +0300

    feat: add new pages and components for enhanced navigation and layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route index element={<HomePage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>Welcome to the app!</p>} />
+          <Route path="cities" element={<p>Cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,24 @@
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import styles from "./SideBar.module.css";
+import { Outlet } from "react-router-dom";
+
+function SideBar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+
+      <footer className={styles.footer}>
+        <p className={styles.copyRight}>
+          &copy; Copyright {new Date().getFullYear()} Your Company. All rights
+          reserved.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default SideBar;
