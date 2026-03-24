a1407ed50faf31f11c84bf876a01cbe81cf0035a
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-03-24 17:42:18 +0300

    feat: implement geolocation functionality in Form and Map components with useUrlPosition hook

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,11 @@
-// "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./Form.module.css";
 import Button from "../Button/Button";
 import ButtonBack from "../ButtonBack/ButtonBack";
+import { useUrlPosition } from "../../hooks/useUrlPosition";
+import Message from "../Message/Message";
+import Spinner from "../Spinner/Spinner";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -14,12 +15,47 @@ export function convertToEmoji(countryCode) {
   return String.fromCodePoint(...codePoints);
 }
 
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
+
 function Form() {
+  const [lat, lng] = useUrlPosition();
+  const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [emoji, setEmoji] = useState("");
+  const [geocodingError, setGeocodingError] = useState(null);
 
+  useEffect(() => {
+    async function fetchCityData() {
+      try {
+        setIsLoadingGeocoding(true);
+        setGeocodingError("");
+        const response = await fetch(
+          `${BASE_URL}?latitude=${lat}&longitude=${lng}`,
+        );
+        const data = await response.json();
+
+        if (!data.countryCode)
+          throw new Error("No country found for the given coordinates");
+        setCityName(data.city || data.locality || "");
+        setCountry(data.countryName);
+        setEmoji(convertToEmoji(data.countryCode));
+      } catch (error) {
+        console.log(error);
+        setGeocodingError(error.message);
+      } finally {
+        setIsLoadingGeocoding(false);
+      }
+    }
+
+    fetchCityData();
+  }, [lat, lng]);
+
+  if (geocodingError) return <Message message={geocodingError} />;
+
+  if (isLoadingGeocoding) return <Spinner />;
   return (
     <form className={styles.form}>
       <div className={styles.row}>
@@ -29,7 +65,7 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        {/* <span className={styles.flag}>{emoji}</span> */}
+        <span className={styles.flag}>{emoji}</span>
       </div>
 
       <div className={styles.row}>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -12,19 +12,17 @@ import { useEffect, useState } from "react";
 import { useCities } from "../../context/CitiesContext";
 import { useGeolocation } from "../../hooks/useGeolocation";
 import Button from "../Button/Button";
+import { useUrlPosition } from "../../hooks/useUrlPosition";
 
 function Map() {
   const { cities } = useCities();
   const [position, setPosition] = useState([51.505, -0.09]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
-
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(() => {
     if (mapLat && mapLng) setPosition([mapLat, mapLng]);
